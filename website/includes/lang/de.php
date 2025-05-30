efc5ffb816c5871da4cb64590d281dcd91292b55
juliandieskau <traumaam@gmail.com>
2025-05-30 20:05:36 +0200

    Added German Translation to first Japan blogpost.

--- website/includes/lang/de.php
+++ website/includes/lang/de.php
@@ -49,7 +49,7 @@ $lang = [
     "username_placeholder" => "Max. 30 Zeichen",
     "username_cancel" => "Abbrechen",
     "username_save" => "Speichern",
-    "username_alert" => "Benutzername muss 1–30 Zeichen lang sein.",
+    "username_alert" => "Benutzername muss 1-30 Zeichen lang sein.",
 
     // PROJECTS
     // Cafemenu
@@ -76,4 +76,44 @@ $lang = [
     "rothkopainting_heading" => "Rothko Gemälde (in Arbeit)",
     "rothkopainting_text" => "freeCodeCamp.org-Projekt um HTML und CSS zu lernen. Ich habe die Übung hier nachgemacht und Kommentare ergänzt, um zu erklären, wie alles funktioniert. Das Projekt beinhaltet ein index.html und ein styles.css, die ein Gemälde erstellen, das im Stil einem Rothko-Gemälde ähnelt.",
     "rothkopainting_files" => "Der Quellcode des Gemäldes ist hier zu sehen:",
+
+    // TRAVEL
+    // JAPAN 24
+    // Day 0: Arrival in Tokyo
+    "japan_day0arrival_heading" => "Ankunft in Tokio - Japanreise 2024",
+    "japan_day0arrival_preface" => "Ich habe begonnen, auf Instagram Bilder von meiner letztjährigen Japanreise zu posten, aber es hat unglaublich viel Zeit gekostet, passende Texte dazu zu schreiben. 
+            Ich wollte meine Erfahrungen wirklich gerne teilen - auch, um sie für mich selbst festzuhalten und mich später daran erinnern zu können. 
+            Aber das Format auf Instagram war einfach nicht ideal: Man kann kein Bild direkt dem Text zuordnen, und die Zeichenbegrenzung machte es schwer, meine Gedanken vollständig auszudrücken. 
+            Da man außerdem nur Bilder im gleichen Format hochladen kann, habe ich mich entschieden, meine Geschichten stattdessen hier mit euch zu teilen.",
+    "japan_day0arrival_p1" => "Zum ersten Mal alleine zu fliegen - und dann direkt auf die andere Seite der Welt - hat mir ehrlich gesagt Angst gemacht. Und das zurecht. 
+            Ein Monat Vorbereitung hätte mich nie auf den Schock vorbereiten können, irgendwo Fremdes anzukommen und ganz auf sich allein gestellt zu sein, bis man selbst zurechtkommt. 
+            Ich kann es wirklich jedem empfehlen - es hilft einem enorm, persönlich zu wachsen.",
+    "japan_day0arrival_p2" => "Japan selbst war nicht so ein 'Kulturschock', wie viele es immer darstellen. 
+            Es war immer noch einfach nur eine Stadt, mit Menschen. Nur eine andere Sprache, anderes Essen und andere Gewohnheiten - aber für mich fühlte es sich dennoch vertraut an. 
+            Vielleicht klingt es überheblich zu sagen, dass meine Denk- und Verhaltensweise gut mit der japanischen harmoniert, aber für mich fühlte es sich genau so an.",
+    "japan_day0arrival_p3" => "Vielleicht lag es an den Jahren, in denen ich langsam ein bisschen Japanisch, die Kultur und Gewohnheiten gelernt habe, und die auch mein Verhalten beeinflusst haben. 
+            Vielleicht war es aber auch etwas anderes. Dieses Gefühl von Ruhe und Reinheit habe ich über die gesamte Reise hinweg sehr genossen.",
+    "japan_day0arrival_p4" => "Ich konnte schon am ersten Tag viele Dinge genießen, die ich jetzt schon vermisse. 
+            Angefangen bei den Verkaufsautomaten an jeder Ecke - nicht nur in der Stadt. Die verschiedenen Softdrinks wie Calpis Soda, weiße Pfirsichlimonade, kalter grüner Tee oder einfach Kaffee - 
+            die Bequemlichkeit, sich keine Getränke für den ganzen Tag einpacken zu müssen, weil man überall günstig welche findet, war einfach großartig.",
+    "japan_day0arrival_p5" => "Und dann die Convenience Stores… Der Name ist absolut gerechtfertigt. Sie bieten nicht nur eine große Auswahl an kalten Getränken und verzehrfertigen Mahlzeiten in allen Varianten (auch gesunde!), 
+            sondern am meisten überrascht haben mich die 'heißen Kühlschränke', die heiße Getränke wie Kaffee oder Tee bereithalten, aber wie ganz normale Kühlschränke aussehen. 
+            Einfach spontan einen heißen Kaffee statt einem kalten mitnehmen - wenn man gerade Lust darauf hatte...",
+    "japan_day0arrival_p7" => "Bei der Ankunft ging meine Pechsträhne weiter: Nachdem ich den Weg zur Zugstation am Flughafen nicht gefunden habe, blieb mir nur noch eine Stunde im Hostel vor dem Treffen mit der Reisegruppe. 
+            In der Zeit musste ich das Anmeldeformular ausfüllen und mein Gepäck im Zimmer verstauen. Zum Glück traf ich dann eines der anderen Gruppenmitglieder, das mir half, nachdem ich minutenlang versucht hatte, mit dem erhaltenen Code die Zimmertür zu öffnen. 
+            Und als ob es nicht schlimmer kommen könnte, bekam ich dann mit nur noch 20 Minuten Zeit auch noch Nasenbluten. Komplett gestresst wartete ich, bis es aufhörte, zog mir schnell ein frisches T-Shirt an und eilte zum Gruppentreffen.",
+    "japan_day0arrival_p8" => "Danach gingen wir zusammen in ein kleines japanisches Restaurant. Ich merkte, dass ich die Hälfte der Getränkekarte lesen konnte, weil sie in Katakana geschrieben war - also fing ich an, für meine Mitreisenden zu übersetzen, 
+            die außer einem schnellen 'Konnichi wa' und 'Arigatou' kein Japanisch konnten. Es hat total Spaß gemacht, mit allen zu sprechen, zu essen und sie kennenzulernen. Wir haben uns sofort gut verstanden.",
+    "japan_day0arrival_p9" => "Das enge Hostelzimmer mit sechs Leuten und kaum Platz, den Koffer zu öffnen, war eine echte Herausforderung. Einer musste sogar auf den Flur gehen, nur um seine Zahnbürste herauszuholen. 
+            Eine komplett andere Umgebung als gewohnt. Aber durch die vielen Menschen aus aller Welt, die ich dort getroffen habe, fing ich schon an, die Zeit richtig zu genießen.",
+    "japan_day0arrival_c1" => "Ankunft am Flughafen Narita",
+    "japan_day0arrival_c2" => "Erster Blick auf die fremde Stadt nach dem Verlassen der U-Bahn",
+    "japan_day0arrival_c3" => "Typischer Strommast in Ginza, Tokio",
+    "japan_day0arrival_c4" => "Erster Blick auf einen der vielen ikonischen Verkaufsautomaten",
+    "japan_day0arrival_c5" => "Tiefkühl-Obst und -Gemüseauswahl in einem 7Eleven",
+    "japan_day0arrival_c6" => "'Heißer Kühlschrank' mit warmen Getränken wie Kaffee und Tee",
+    "japan_day0arrival_c7" => "Blick aus dem Hostelzimmer",
+    "japan_day0arrival_c8" => "Erstes Abendessen mit den Mitgliedern meiner Reisegruppe: Nur eine Calpis Soda, etwas Edamame und Pommes - einfach, aber lecker",
+    "japan_day0arrival_c9" => "Blick auf das Hostelzimmer von meinem Bett aus",
+
 ];
